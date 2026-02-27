30e9cc2b9cf8eb95c68cafee7242a2efefcfcd14
Yashu Patel <129998889+Splendor73@users.noreply.github.com>
2026-02-27 16:38:13 -0700

    chore: prep for fresh subtree

--- osomba/mobile/lib/utils/constants.dart
+++
@@ -1,7 +0,0 @@
-class AppConstants {
-  static const String baseUrl = 'http://localhost:8000/api/v1';
-  static const String authEndpoint = '/auth';
-  static const String loginEndpoint = '/auth/login';
-  static const String registerEndpoint = '/auth/register';
-  static const String meEndpoint = '/auth/me';
-}
